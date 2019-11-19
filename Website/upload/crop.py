ea20dc4b9321162bbe662d6bfb94a036755cf8c3
Mark Gatongay <antoniogatongay@Royal-Rikus-MacBook-Pro.local>
2019-11-19 18:51:59 -0500

    modified cropper

--- Website/upload/crop.py
+++ Website/upload/crop.py
@@ -3,15 +3,13 @@ from PIL import Image
 # Replace with converted image
 im = Image.open('TestPic.jpeg')
 
-def crop_center(pil_img, crop_width, crop_height):
+def crop_center(pil_img):
+    minimum = min(pil_img.size)
     img_width, img_height = pil_img.size
-    return pil_img.crop(((img_width - crop_width) // 2,
-                         (img_height - crop_height) // 2,
-                         (img_width + crop_width) // 2,
-                         (img_height + crop_height) // 2))
+    return pil_img.crop(((img_width - minimum) // 2,
+                         (img_height - minimum) // 2,
+                         (img_width + minimum) // 2,
+                         (img_height + minimum) // 2))
 
-def crop_max_square(pil_img):
-    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
-
-im_new = crop_max_square(im)
+im_new = crop_center(im)
 # im_new.save('cropped.jpg', quality=100)
