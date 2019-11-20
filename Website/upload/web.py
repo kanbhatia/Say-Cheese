9ba4a3795a78f4a0f063e83225d998a610d06762
Mark Gatongay <antoniogatongay@Royal-Rikus-MacBook-Pro.local>
2019-11-19 20:10:56 -0500

    routed flask htmls

--- Website/upload/web.py
+++ Website/upload/web.py
@@ -4,13 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template("homepage.html")
+
+@app.route("/camera")
+def camera():
     return render_template("index.html")
+
+@app.route("/engine")
+def engine():
+    return render_template("engine.html")
 
 @app.route('/data', methods=['POST'])
 def handle_data():
     encoded = request.form['datauri']
     print(encoded)
-    return redirect("/")
+    return redirect("/camera")
 
 
 if __name__ == "__main__":
